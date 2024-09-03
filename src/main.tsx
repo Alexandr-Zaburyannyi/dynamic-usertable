a320323f49150fee6b773c05a06db09a0ac41493
Alexander-Zaburiannyi <ozaburiannyi@gmail.com>
2024-09-03 12:12:06 +0200

    feat:minor changes were provided

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 /** @format */
 
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { MantineProvider } from '@mantine/core';
@@ -9,11 +8,9 @@ import '@mantine/core/styles.css';
 import { store } from './store/store.ts';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <MantineProvider>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </MantineProvider>
-  </StrictMode>
+  <MantineProvider>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </MantineProvider>
 );
