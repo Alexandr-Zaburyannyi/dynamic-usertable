bc4981165f68fb734670fb5f10e8d4e5e82d7918
Alexander-Zaburiannyi <ozaburiannyi@gmail.com>
2024-09-03 11:43:17 +0200

    feat:minor changes were provided

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,12 @@ import axios from 'axios';
 import { setUsers } from './store/slices/usersSlice';
 import UsersTable from './components/UsersTable';
 import { gradientColor } from './config/constants';
+import { useMediaQuery } from '@mantine/hooks';
 
 function App() {
   const dispatch = useDispatch();
+
+  const min = useMediaQuery('(min-width: 700px)');
 
   useEffect(() => {
     const fetchUsers = async () => {
@@ -27,7 +30,11 @@ function App() {
       align='center'
       direction='column'
       w={'100%'}
-      style={{ overflowX: 'auto' }}
+      h={'100vh'}
+      style={{
+        overflow: 'visible',
+        transform: min ? 'scale(1)' : 'scale(0.8)',
+      }}
     >
       <Text
         tt='capitalize'
